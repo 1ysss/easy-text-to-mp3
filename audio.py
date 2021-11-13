a5a80c3a6fc9535c24f569d3f27452e1c0d037ab
Max1m1a <86849404+Max1m1a@users.noreply.github.com>
2021-11-13 18:51:15 +0300

    Create audio.py

---
+++ audio.py
@@ -0,0 +1,25 @@
+import pyttsx3
+from gtts import gTTS
+from tqdm import tqdm
+from time import sleep
+
+engine = pyttsx3.init()
+
+file_path = input( "Введите путь к файлу:  ")
+
+file = open( file_path, 'r')
+
+theText = file.read()
+
+engine.say( theText )
+engine.runAndWait()
+
+file.close()
+
+for i in tqdm( range( 100 ) ):
+	sleep( 0.01 )
+
+tts = gTTS ( text = theText, lang = 'ru' )
+tts.save( 'youfile.mp3' )
+
+print( 'File Saved...' )
